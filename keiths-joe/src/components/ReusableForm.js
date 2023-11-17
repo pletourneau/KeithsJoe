65bf49e88efc305b34dab59f7993508ed4efd728
Paul LeTourneau <pletourneau62@gmail.com>
2023-11-17 09:19:41 -0800

    update reusable form

--- keiths-joe/src/components/ReusableForm.js
+++ keiths-joe/src/components/ReusableForm.js
@@ -5,9 +5,17 @@ function ReusableForm(props) {
   return (
     <React.Fragment>
       <form onSubmit={props.formSubmissionHandler}>
-        <input type="text" name="names" placeholder="Pair Names" />
-        <input type="text" name="location" placeholder="Location" />
-        <textarea name="issue" placeholder="Describe your issue." />
+        <input type="text" name="name" placeholder="Coffee Name" />
+        <input type="text" name="origin" placeholder="Origin" />
+        <input type="number" name="price" placeholder="Price" />
+        <label for="roast">Roast</label>
+        <select name="roast">
+          <option value="light">Light</option>
+          <option value="medium">Medium</option>
+          <option value="dark">Dark</option>
+          <option value="decaf">Decaf</option>
+        </select>
+        <input type="number" name="inventory" placeholder="130" />
         <button type="submit">{props.buttonText}</button>
       </form>
     </React.Fragment>
